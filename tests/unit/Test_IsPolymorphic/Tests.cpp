98eaf8c7639f3355aa4808699aee8f9249528aee
pohilets <pohilets@67599820-88c8-11de-9c77-2f49170b69f2>
2010-01-25 15:59:36 +0000

    +: Copyright disclaimer added to source files
    git-svn-id: http://cpp-events.googlecode.com/svn/trunk@151 67599820-88c8-11de-9c77-2f49170b69f2

--- tests/unit/Test_IsPolymorphic/Tests.cpp
+++ tests/unit/Test_IsPolymorphic/Tests.cpp
@@ -8,6 +8,28 @@ using Cpp::Private::Events::IsPolymorphic;
 // For checking polymorphy special template class is derived from class being tested.
 // This potentially may cause compilation problems if some special members of base class are inaccessible.
 // This test is designed for detecting these problems. Test is passed if it compiles.
+// Copyright (c) 2010 Nickolas Pohilets
+//
+// This file is a part of the unit test suit for the CppEvents library.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 TEST(Test_IsPolymorphic, Compilability)
 {
 	bool const b = IsPolymorphic<ClassWithUndefinedPrivateCtorAndDtor>::value;
