9027a0d91a983c680d3a52b12a0083a22744d1cd
pohilets <pohilets@67599820-88c8-11de-9c77-2f49170b69f2>
2010-01-30 01:27:51 +0000

    -: Fixed: missing copyright notice in 'Cpp/Events.hpp'
    git-svn-id: http://cpp-events.googlecode.com/svn/trunk@189 67599820-88c8-11de-9c77-2f49170b69f2

--- src/Cpp/Events.hpp
+++ src/Cpp/Events.hpp
@@ -1,8 +1,28 @@
-#ifndef CPP__EVENTS__HPP
-#define CPP__EVENTS__HPP
-
+// Copyright (c) 2010 Nickolas Pohilets
+//
 // This file is a compile-time entry point into the library.
 // Include it to use all the stuff provided by the library.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef CPP__EVENTS__HPP
+#define CPP__EVENTS__HPP
 
 #include <Cpp/Events/ConnectionScope.hpp>
 
